af75c98038ba2455cc3216a76670ae826368c7c6
Tryfon Emmanouilidis <tryfon.emmanouilidis@eurodyn.com>
2016-08-17 13:51:20 +0300

    Fixed view-atom swaps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-tetris "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT licence"
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clj-tetris.swing
   :target-path "target/%s"
